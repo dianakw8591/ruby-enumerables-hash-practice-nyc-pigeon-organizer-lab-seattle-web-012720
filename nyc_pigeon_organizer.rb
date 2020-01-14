81f452025b0cd31d3b5c4fd22863e428e5085017
Diana Wendt <dianakw@gmail.com>
2020-01-14 19:30:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  sorted_pigeons = data.reduce({}) do |memo, (data_key, hash)|
+    memo[data_key] = hash.reduce({}) do |memo_2, (quality, name)|
+      i = 0
+      memo_2[name[i]] = []
+      while i < name.length do
+        memo_2[name[i]] << quality.to_s
+        i += 1
+        p memo_2
+      end
+    #hash.reduce({}) do |memo_2, (quality, name)|
+     # p memo
+      #memo[name][data_key] = [quality.to_s] if !memo[name][data_key] 
+      #memo[name][data_key] << quality.to_s
+      #p memo
+    #end
+    end
+  end
+  memo
 end
