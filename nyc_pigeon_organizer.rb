7c38b1306d74195d68636d7748b2df911f2f3ded
Diana Wendt <dianakw@gmail.com>
2020-01-14 21:59:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,10 @@ def nyc_pigeon_organizer(data)
     hash.reduce(memo) do |memo, (quality, name_array)|
       i = 0
       while i < name_array.length do
-        if (!memo[name_array[i]] || !memo[name_array[i]][data_key])
+        if !memo[name_array[i]]
           memo[name_array[i]] = {data_key => [quality.to_s]}
+        elsif !memo[name_array[i]][data_key]
+          memo[name_array[i]][data_key] = [quality.to_s]
         else
           memo[name_array[i]][data_key] << quality.to_s
         end
