4a8bb1b050f9eac51aaebe3e3e9c50f6fe3103cb
Diana Wendt <dianakw@gmail.com>
2020-01-14 20:05:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,10 +6,12 @@ def nyc_pigeon_organizer(data)
       while i < name_array.length do
         memo[name_array[i]] = memo_2 
         i += 1
+        p memo
         p memo_2
       end
       memo_2[data_key] << quality.to_s
     end
   end
   memo
+  p memo
 end
